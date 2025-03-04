96afc57ade4375445b456f1221fc47ecee569249
Ahmed241998 <125216783+Ahmed241998@users.noreply.github.com>
2025-03-05 00:48:49 +0200

    Update Data.py

--- Data.py
+++ Data.py
@@ -3,7 +3,7 @@ import pandas as pd
 def app() :
     st.title('Data')
     st.header('This Data is Scraped From Spotify API and Youtube API')
-    df=pd.read_csv("Final_Spotify_Youtube_Data.csv")
+    df=pd.read_csv("Streamlit Visualiztion.csv")
     df.drop('Unnamed: 0',axis=1,inplace=True)
     df.dropna(axis=0,inplace=True)
     st.dataframe(df)
